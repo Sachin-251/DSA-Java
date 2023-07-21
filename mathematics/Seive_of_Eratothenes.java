64faf21d8823b3ad479638e2104d2745cf63dae4
Sachin-251 <sachin.251@outlook.com>
2023-07-21 12:43:55 +0530

    21-07-2023

---
+++ mathematics/Seive_of_Eratothenes.java
@@ -0,0 +1,47 @@
+package mathematics;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Seive_of_Eratothenes{
+
+    //Naive Approach
+    // public void seive(int num){
+    //     if(num<=1) return;
+    //     Check_Prime cp = new Check_Prime();
+    //     for(int i=2;i<=num;i++){
+    //         if(cp.checkPrime(i)){
+    //             System.out.print(i+" ");
+    //         }
+    //     }
+    // }
+
+    //Efficient Approach
+    public void seive(int num){
+        if(num<=1) return;
+        boolean isPrime[] = new boolean[num+1];
+        Arrays.fill(isPrime, true);
+        for(int i=2;i*i<=num;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=num;j=j+i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+        for(int i=2;i<=num;i++){
+            if(isPrime[i]){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.println("Enter the number: ");
+        num=sc.nextInt();
+        System.out.println("Prime's are: ");
+        Seive_of_Eratothenes soe = new Seive_of_Eratothenes();
+        soe.seive(num);
+        sc.close();
+    }
+}
