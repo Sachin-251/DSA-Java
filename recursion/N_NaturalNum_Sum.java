4db1e3d78befcffae2c8acef913cce0e737d5a38
Sachin-251 <sachin.251@outlook.com>
2023-08-20 14:12:10 +0530

    recurssion vid 2

---
+++ recursion/N_NaturalNum_Sum.java
@@ -0,0 +1,18 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class N_NaturalNum_Sum {
+    //General Formula = n*(n+1)/2
+    public static int sum(int num){
+        if(num<=1) return 1;
+
+        return num+sum(num-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(sum(num));
+        sc.close();
+    }
+}
